199fd52a555b5902ee408a71aba2d9e68477c347
Douglas Yung <dyung@users.noreply.github.com>
2025-04-24 00:01:29 -0400

    Add first draft of script to create ansible user and add to administrator group.

---
+++ windows/add_ansible_user.cmd
@@ -0,0 +1,19 @@
+@setlocal
+@echo off
+
+set PASSWORD=%~1
+
+IF /I NOT "%PASSWORD%" == "" goto :ValidPassword
+  REM === Display usage message
+  echo Usage: %~nx0 ^<password^>
+  exit /B 1
+
+:ValidPassword
+
+REM === Add the user with the specified password
+call net user ansible "%PASSWORD%" /add /expires:never
+
+REM === Add the ansible user to the Administrators group
+call net localgroup Administrators ansible /add
+
+endlocal
