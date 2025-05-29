f35c7b656777941e4e5b68818a6ca4f17d401ef1
Douglas Yung <dyung@users.noreply.github.com>
2025-05-28 22:52:57 -0400

    Enable screen sharing.

--- MacOS/bootstrap_macOS.sh
+++ MacOS/bootstrap_macOS.sh
@@ -39,6 +39,10 @@ pip3 install ansible
 # Enable ssh access
 sudo systemsetup -setremotelogin on
 
+# Enable screen sharing
+sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool false
+sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
+
 # Create the ssh directories
 mkdir ~/.ssh
 chmod 700 ~/.ssh
